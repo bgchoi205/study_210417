4046cfcfbd0c926be2e1998709a3f35e39b44156
bgchoi205 <choibg205@naver.com>
2021-04-17 19:22:44 +0900

    java_study

---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int month = 4;
+        String season = "";
+
+
+        switch (month){
+            case 1:
+                season = "겨울";
+                break;
+            case 2:
+                season = "겨울";
+                break;
+            case 3:
+                season = "봄";
+                break;
+            case 4:
+                season = "봄";
+                break;
+            case 5:
+                season = "봄";
+                break;
+            case 6:
+                season = "여름";
+                break;
+            case 7:
+                season = "여름";
+                break;
+            case 8:
+                season = "여름";
+                break;
+            case 9:
+                season = "가을";
+                break;
+            case 10:
+                season = "가을";
+                break;
+            case 11:
+                season = "가을";
+                break;
+            case 12:
+                season = "겨울";
+                break;
+
+        }
+        System.out.println(season);
+
+    }
+}
